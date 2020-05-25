e8fb0f5c89e1e916ee32e72d890ec9db21b496a1
dydtjq94 <62597465+dydtjq94@users.noreply.github.com>
2020-05-25 15:40:24 +0900

    Prestart

--- src/app.js
+++ src/app.js
@@ -5,14 +5,16 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import routes from "./routes";
 import router from "./routers/router";
+import path from "path";
 
 const app = express();
 
 // view engine pug 사용
 app.set(`view engine`, "pug");
+app.set("views", path.join(__dirname, "views"));
 
 // static 폴더 추가하여 css, js 적용
-app.use("/static", express.static("static"));
+app.use("/static", express.static(path.join(__dirname, "static")));
 app.use("/storage", express.static("storage"));
 
 // middleware 설정
