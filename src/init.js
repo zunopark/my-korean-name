8b27e42a8f8a402195cdc7564472eae3bbdb4eb1
dydtjq94 <62597465+dydtjq94@users.noreply.github.com>
2020-05-25 16:53:43 +0900

    mobile

--- src/init.js
+++ src/init.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import app from "./app";
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 const handleListening = () =>
   console.log(`😻LISTENING ON: https://localhost:${PORT}`);
