49b70eda47172d2b0f3a2acc453cf569182bf748
dydtjq94 <62597465+dydtjq94@users.noreply.github.com>
2020-05-25 15:54:46 +0900

    @BABEL

--- src/init.js
+++ src/init.js
@@ -3,6 +3,6 @@ import app from "./app";
 const PORT = 5000;
 
 const handleListening = () =>
-  console.log(`😻LISTENING ON: https://localhost:${PORT}`);
+  console.log(`😻LISTENING ON: https://localhost:5000`);
 
 app.listen(PORT, handleListening);
